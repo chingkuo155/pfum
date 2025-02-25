950d3b77bb2d2ba0f2eb2c199299974cfb818337
swift <swift>
2025-02-25 01:49:24 +0000

    Task 9: Create an Endpoint to Add a User

--- crud.py
+++ crud.py
@@ -1,5 +1,5 @@
 from sqlalchemy.orm import Session
-import models as models
+import models as models, schemas
 
 def get_users(db: Session, skip: int = 0, limit: int = 100):
     return db.query(models.User).offset(skip).limit(limit).all()
@@ -9,3 +9,20 @@ def get_user(db: Session, user_id: int):
 
 def get_user_by_email(db: Session, email: str):
     return db.query(models.User).filter(models.User.email == email).first()
+
+def create_user(db: Session, user: schemas.UserCreate):
+    fake_hashed_password = hash(user.password)
+    db_user = models.User(
+        email=user.email,
+        hashed_password=fake_hashed_password,
+        username=user.username,
+        first_name=user.first_name,
+        last_name=user.last_name,
+        gender=user.gender,
+        country=user.country,
+        isActive=user.isActive
+    )
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return db_user
